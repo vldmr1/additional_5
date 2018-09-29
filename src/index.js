b9b2bc1e62d1c1eacd7f4c524afb53a09d9f5a9c
Vladimir Malygin <43152051+vldmr1@users.noreply.github.com>
2018-09-29 09:52:35 +0300

    additional-5 solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  for (let symbol of str) {
+    for (let subarray of bracketsConfig) { 
+      if (symbol === subarray[0]) {
+        if (subarray[0] === subarray[1] && stack[stack.length - 1] === subarray[0]) {
+          stack.pop();
+        } else {
+          stack.push(symbol);
+        }  
+      } else {
+        if (symbol === subarray[1] && stack[stack.length - 1] !== subarray[0]) {
+            return false
+        }  
+        else if (symbol === subarray[1] && stack[stack.length - 1] === subarray[0]) {
+          stack.pop();          
+        }
+      }
+    }  
+  }
+return (stack.length === 0 ? true : false);
 }
